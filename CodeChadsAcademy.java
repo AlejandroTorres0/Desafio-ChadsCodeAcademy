1f10cde6b1c6d6f3a1d0c9d6e85cfdd29863d83d
Alejandro <at7709739@gmail.com>
2025-10-02 14:28:08 -0300

    primer commit

---
+++ CodeChadsAcademy.java
@@ -0,0 +1,17 @@
+public class ChadsCodeAcademy() {
+	public static void main(String[] args) {
+		int[] notaPorExamen = new int[];
+		int total = 0;
+
+		notaPorExamen[0] = 80;
+		notaPorExamen[1] = 60;
+		notaPorExamen[2] = 50;
+
+		
+	}
+
+
+
+
+
+}
